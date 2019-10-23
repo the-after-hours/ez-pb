1fa8e9238b6cad005a2fc553c1b2549a1252bee8
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-10-22 22:58:25 -0700

    Add placeholder functions for main app

--- ezpb/__main__.py
+++ ezpb/__main__.py
@@ -1,21 +1,57 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 
-class EzPb(App):
+Builder.load_string('''
+<MainMenu>:
+    orientation: 'vertical'
+    size_hint_y: .5
+    Label:
+        text: 'Welcome to the Ez PhotoBooth!'
+    # Label:
+    #     text: root.logs
+    BoxLayout:
+        orientation: 'horizontal'
+        size_hint: (1,.3)
+        Button:
+            text: 'Choose Printer'
+            on_press: root.choose_printer()
+        Button:
+            text: 'Choose Overlay Image'
+            on_press: root.choose_overlay()
+        Button:
+            text: 'Choose Destination Folder'
+            on_press: root.choose_destination()
+        Button:
+            text: 'Start'
+            on_press: root.start()
+''')
+
+class MainMenu(BoxLayout):
+    def choose_printer(self):
+        print("TODO: Find Printer")
+
+    def choose_overlay(self):
+        print("TODO: Select overlay image")
+
+    def choose_destination(self):
+        print("TODO: Set destination directory")
+
+    def start(self):
+        print("TODO: Start watching")
+
+class Photobooth(App):
     def build(self):
-        layout = BoxLayout(
-            orientation='vertical')
+        return MainMenu()
 
-        welcome = Label(text='Welcome to the Ez PhotoBooth!')
+class Notifications(BoxLayout):
+    def __init__(self):
+        self.message = 'Welcome to the Ez Photobooth'
 
-        start = Button(text='Start', size_hint=(.2, .2))
-        
-        layout.add_widget(welcome)
-        layout.add_widget(start)
-
-        return layout
+    def notify(self, message):
+        self.message = message
 
 if __name__ == '__main__':
-    EzPb().run()
+    Photobooth().run()
